9bd625cc585f53c4ab09a81e954ede35d30cc216
ZhibeiUkong <149119240+ZhibeiUkong@users.noreply.github.com>
2023-12-06 20:21:13 -0800

    Add files via upload

--- Data Cleaning Project.R
+++ Data Cleaning Project.R
@@ -4,6 +4,8 @@ library(testthat)
 library(ggplot2)
 library(shiny)
 library(DT)
+library(tidyr)
+
 df4 <- read.csv("PER_CAPITA_INCOME_and_AGGREGATE_INCOME_IN_THE_PAST_12_MONTHS_(IN_INFLATION-ADJUSTED_DOLLARS).csv")
 df10 <- read.csv("TRAVEL_TIME_TO_WORK_(B08303).csv")
 Per_capita_income_df<-df4
@@ -55,8 +57,6 @@ df_Plus <- Per_capita_income_and_influencing_factors %>%
   filter(!is.na(Changes_in_the_average_commute_time) & !is.na(Trends_in_the_economy))
 df_name <- df_Plus %>%
   distinct(GEOID, NAME)
-library(shiny)
-
 ui <- fluidPage(
   titlePanel("Data Analysis Dashboard"),
   
@@ -78,14 +78,36 @@ ui <- fluidPage(
   )
 )
 server <- function(input, output) {
-
-  output$commutePlot <- renderPlot({
+  
+output$commutePlot <- renderPlot({
 
     selected_data <- filter(df_Plus, GEOID == input$geoID, ACS_VINTAGE == input$vintage)
-    ggplot(selected_data, aes(x = 1, y =1 )) +
-      geom_bar(stat = "identity")
-  })
-  
+
+    selected_data_long <- pivot_longer(selected_data, 
+                                       cols = starts_with("B08303_"), 
+                                       names_to = "Commute_Category", 
+                                       values_to = "Count")
+    commute_labels <- c(B08303_001E = "Total popular",
+      B08303_002E = "Less than 5 minutes commute",
+                        B08303_003E = "5 to 9 minutes commute",
+                        B08303_004E = "10 to 14 minutes commute",
+                        B08303_005E = "15 to 19 minutes commute",
+                        B08303_006E = "20 to 24 minutes commute",
+                        B08303_007E = "25 to 29 minutes commute",
+                        B08303_008E = "30 to 34 minutes commute",
+                        B08303_009E = "35 to 39 minutes commute",
+                        B08303_010E = "40 to 44 minutes commute",
+                        B08303_011E = "45 to 59 minutes commute",
+                        B08303_012E = "60 to 89 minutes commute",
+                        B08303_013E = "90 or more minutes commute")
+    
+    selected_data_long$Commute_Category <- recode(selected_data_long$Commute_Category, !!!commute_labels)
+
+    ggplot(selected_data_long, aes(x = Commute_Category, y = Count)) +
+      geom_bar(stat = "identity") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+      labs(x = "Commute Time", y = "Number of People", title = "Commute Time Distribution")
+})
   output$commuteTable <- renderDT({
 
     selected_data <- filter(df_Plus, GEOID == input$geoID, ACS_VINTAGE == input$vintage)
@@ -105,8 +127,16 @@ server <- function(input, output) {
 
     selected_geo_data <- filter(df_Plus, GEOID == input$geoIDWage)
 
-    ggplot(selected_geo_data, aes(x = 1, y =1)) + 
-      geom_bar(stat = "identity")
+    selected_data_long <- pivot_longer(selected_geo_data, 
+                                       cols = B19301_001E, 
+                                       names_to = "Year", 
+                                       values_to = "PerCapitaIncome")
+
+    ggplot(selected_data_long, aes(x = ACS_VINTAGE, y = PerCapitaIncome)) + 
+      geom_bar(stat = "identity") +
+      labs(x = "Year", y = "Per Capita Income (Inflation-Adjusted)") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
   })
 }
+
 shinyApp(ui, server)
